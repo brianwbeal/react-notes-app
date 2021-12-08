054edde31310270be5c3eb7bed1533201aa60dbd
Brian Beal <brianwbeal@gmail.com>
2021-12-07 19:11:36 -0800

    Set up initial project using create-react-app, clearing out boilerplate and adding NotesApp component.

---
+++ src/NotesApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class NotesApp extends React.Component {
+  render() {
+    return (
+      <div className="NotesApp">
+        <h1>React Notes App</h1>
+      </div>
+    )
+  }
+}
+
+export default NotesApp;
